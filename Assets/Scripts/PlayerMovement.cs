2760e83c90783397af2433828ae7e473c4e937f4
Vlad <vladik11012006@gmail.com>
2025-01-23 13:46:20 +0100

    moveemnt

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 direction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                transform.position += transform.right * 5f;
+            }
+            else
+            {
+                Vector3 a = transform.eulerAngles;
+                a.y += 90f;
+                transform.eulerAngles = a;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                transform.position += -transform.right * 5f;
+            }
+            else
+            {
+                Vector3 a = transform.eulerAngles;
+                a.y -= 90f;
+                transform.eulerAngles = a;
+            }
+        }
+        rb.velocity = transform.forward * speed;
+    }
+}
